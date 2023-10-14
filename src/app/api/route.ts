80224d0826c9100ad1199dd5557df4517d655a3f
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2023-10-14 16:12:36 +0200

    prisma database integration

---
+++ src/app/api/route.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client'
+import { NextResponse } from 'next/server'
+const prisma = new PrismaClient()
+
+export async function GET(request: Request , response : NextResponse) {
+    const allUsers = await prisma.users.findMany()
+    console.log(allUsers)
+    console.log("get")
+    return NextResponse.json({ yes: 'great success' ,allUsers}, { status: 200 })
+}
