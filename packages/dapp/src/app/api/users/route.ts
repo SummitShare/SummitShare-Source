0a87921a749974f7aad04129ca2bbf517de555d2
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2023-11-30 13:40:58 +0200

    API

--- packages/dapp/src/app/api/users/route.ts
+++ packages/dapp/src/app/api/users/route.ts
@@ -1,10 +1,10 @@
-// import { PrismaClient } from '@prisma/client'
-// import { NextResponse } from 'next/server'
-// const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client'
+import { NextResponse } from 'next/server'
+const prisma = new PrismaClient()
 
-// export async function GET(request: Request , response : NextResponse) {
-//     const allUsers = await prisma.users.findMany()
-//     console.log(allUsers)
-//     console.log("get")
-//     return NextResponse.json({ yes: 'great success' ,allUsers}, { status: 200 })
-// }
+export async function GET(request: Request , response : NextResponse) {
+    const allUsers = await prisma.users.findMany()
+    console.log(allUsers)
+    console.log("get")
+    return NextResponse.json({ yes: 'great success' ,allUsers}, { status: 200 })
+}
