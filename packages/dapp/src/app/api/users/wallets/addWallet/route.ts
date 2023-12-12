838a398a30c67e658b3c406f03432ba5315a3312
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2023-12-12 12:41:14 +0200

    add wallet api

---
+++ packages/dapp/src/app/api/users/wallets/addWallet/route.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+export async function POST(req: Request, res: NextResponse) {
+    const requestBody = await req.json();
+    const { user_id, wallet_address }: { user_id: string, wallet_address: string } = requestBody;
+
+    try {
+        if (!user_id || !wallet_address) {
+            return NextResponse.json({ error: 'Missing user_id or wallet_address' }, { status: 400 });
+        }
+
+        // Optionally, add validation for user_id and wallet_address
+
+        const newUserWallet = await prisma.user_wallets.create({
+            data: {
+                user_id: user_id,
+                wallet_address: wallet_address
+            }
+        });
+
+        return NextResponse.json({ newUserWallet }, { status: 201 });
+        
+    } catch (error) {
+        console.error("Error in POST request:", error);
+        return NextResponse.json({ error: 'An error occurred while processing your request.' }, { status: 500 });
+    }
+}
