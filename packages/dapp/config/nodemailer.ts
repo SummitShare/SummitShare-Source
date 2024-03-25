08525b09de41129ced6726f0fd6aeee5ffb46df8
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2024-03-25 15:03:48 +0200

    email clean up

--- packages/dapp/config/nodemailer.ts
+++ packages/dapp/config/nodemailer.ts
@@ -2,19 +2,19 @@
 import nodemailer from "nodemailer";
 
 export const emailServer = process.env.SEMAIL
-const newemail = "thomas.gondwe@bicos.io"
+// const newemail = "thomas.gondwe@bicos.io"
 
-const pass = process.env.S_APP_PASSWORD
+const password = process.env.S_APP_PASSWORD
 
 export const transporter = nodemailer.createTransport({
     service: "gmail",
     auth:{
         user: emailServer,
-        pass
+        pass: password,
     }
 
 }) 
-export const mailOptions = {
-    from: emailServer,
-    to: newemail,
-}
+// export const mailOptions = {
+//     from: emailServer,
+//     to: newemail,
+// }
