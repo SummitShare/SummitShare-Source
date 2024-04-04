0e28feb4cdd143e2acf4f2513bf39a39e8c8ea71
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2024-04-04 16:34:50 +0200

    user verification api update and page second change

--- packages/dapp/src/app/(auth)/verifcation/email/[token]/page.tsx
+++ packages/dapp/src/app/(auth)/verifcation/email/[token]/page.tsx
@@ -107,10 +107,6 @@ import { useEffect, useState } from "react";
             <button  onClick={()=>resendVerificationEmail} className="ring-1 ring-gray-300 rounded-md px-3 py-2 text-xs">navigate</button>
           
           }
-
-
-
-
         </div>
       </div>
     </div>
