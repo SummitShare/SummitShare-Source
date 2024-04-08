ad2df7c2a652bfe91d3f29b9995f77bba9eef401
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2024-04-08 15:15:22 +0200

    update verification token link in the api signup api

--- packages/dapp/src/app/api/v1/signup/route.ts
+++ packages/dapp/src/app/api/v1/signup/route.ts
@@ -32,7 +32,7 @@ async function createSendTokens(user:users,email:string) {
 
   // const host = req.headers.get('host');
   const host = process.env.HOST
-  const verificationLink = `${host}api/v1/user/verifyEmail?token=${token}`;
+  const verificationLink = `${host}api/v1/user/verification/verifyEmail?token=${token}`;
 
   const mailOptions = {
     from: emailServer,
