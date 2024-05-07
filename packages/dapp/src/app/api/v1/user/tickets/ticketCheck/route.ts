c4ad5c5852416b9d684e4fbec5edcc91c251c56b
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2024-05-07 15:45:17 +0200

    ticket validation for virtual

---
+++ packages/dapp/src/app/api/v1/user/tickets/ticketCheck/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server';
+import bcrypt from 'bcryptjs';
+import crypto from 'node:crypto';
+import prisma from '../../../../../../../config/db';
+
+
+
+export async function POST(req: Request, res: NextResponse) {
+  try {
+    const { user_id, event_id } = await req.json();
+
+    if (!event_id) {
+      return NextResponse.json({ message: "no event id sent", }, { status: 400 });
+    }
+    if (!user_id) {
+      return NextResponse.json({ message: "no user id sent", }, { status: 400 });
+    }
+
+    const ticket = prisma.tickets.findFirst({
+      where:{
+        user_id,
+        event_id
+      }
+    })
+
+    if(!ticket){
+      return NextResponse.json({ allowed:"false"}, { status: 401 });
+    }
+
+
+    return NextResponse.json({ allowed:"true"}, { status: 401 });
+
+
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ failure: error }, { status: 500 });
+  }
+}
+
+
