a58f02e4f3fb0358f8225b0d4ab8ce58b23c9489
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2024-05-07 14:54:58 +0200

    ticket creation and ticket transaction recording

--- packages/dapp/src/app/api/v1/events/tickets/create/route.ts
+++ packages/dapp/src/app/api/v1/events/tickets/create/route.ts
@@ -2,6 +2,7 @@ import { IPropsal, EmailArray } from '@/utils/dev/typeInit';
 import { PrismaClient, } from '@prisma/client'
 import { NextResponse } from 'next/server'
 import prisma from '../../../../../../../config/db';
+import { randomUUID } from 'node:crypto';
 
 
 
@@ -12,7 +13,7 @@ export async function POST(req: Request , res : NextResponse) {
 
     try {
 
-        const {wallet_address , event_id } = await req.json()
+        const {wallet_address , event_id, user_id }  = await req.json()
 
         if(!wallet_address){
             return NextResponse.json({ message: "no wallet address sent", }, { status: 400 });
@@ -20,16 +21,50 @@ export async function POST(req: Request , res : NextResponse) {
         if(!event_id){
             return NextResponse.json({ message: "no event id sent", }, { status: 400 });
         }
+        if(!user_id){
+            return NextResponse.json({ message: "no event id sent", }, { status: 400 });
+        }
 
         const ticket = await prisma.tickets.create({
             data:{
                 wallet_address: wallet_address,
                 event_id: event_id,
+                user_id:user_id
+            }
+        })
+        if(!ticket){
+            return NextResponse.json({ message: "failed to create ticket", }, { status: 400 });
+        }
+
+        const event = await prisma.events.findUnique({
+            where:{
+                id: event_id
+            }
+        })
+        if(!event){
+            return NextResponse.json({ message: "no event found", }, { status: 400 });
+        }
+
+
+        const price = event.cost!
+
+        if(!user_id){
+            return NextResponse.json({ message: "no user di id sent", }, { status: 400 });
+        }
+
+        const id = randomUUID()
+        
+        const ticketTransaction = await prisma.ticket_transaction.create({
+            data:{
+                id,
+                event_id,
+                user_id,
+                price
             }
         })
 
-        if(!ticket){
-            return NextResponse.json({ message: "failed to create ticket", }, { status: 400 });
+        if(!ticketTransaction){
+            return NextResponse.json({ message: "transaxtion not recorded", }, { status: 500 });
         }
 
         return NextResponse.json({ message: "ticket created", }, { status: 200 });
