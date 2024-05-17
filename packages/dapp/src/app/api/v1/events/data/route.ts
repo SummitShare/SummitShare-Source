012948061773006eddace9c81e0fc680a348fb35
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2024-05-17 16:07:08 +0200

    stakeholder event information

--- packages/dapp/src/app/api/v1/events/data/route.ts
+++ packages/dapp/src/app/api/v1/events/data/route.ts
@@ -19,7 +19,13 @@ export async function POST(req: Request, res: NextResponse) {
       return NextResponse.json({ message: "no user id sent", }, { status: 400 });
     }
 
-    const ticketTransactions = await prisma.ticket_transaction.count()
+    const ticketsSold = await prisma.ticket_transaction.findMany({
+      where:{
+        event_id:event_id
+      }
+    })
+
+    const ticketTransactions = ticketsSold.length
 
     const event = await prisma.events.findUnique({
       where: {
@@ -31,11 +37,53 @@ export async function POST(req: Request, res: NextResponse) {
       return NextResponse.json({ message: "no event found", }, { status: 400 });
     }
 
+    const stakeholders = await prisma.stakeholders.findMany({
+      where:{
+        event_id:event_id
+      }
+    })
+
+    if (!stakeholders || stakeholders.length === 0) {
+      return NextResponse.json({ message: "No stakeholders found" }, { status: 404 });
+    }
+
+    // Fetch user details for each stakeholder and calculate distribution
+    const distributionData = await Promise.all(stakeholders.map(async (stakeholder) => {
+      if (!stakeholder.user_id) {
+        return null;
+      }
+
+      const user = await prisma.users.findUnique({
+        where: { id: stakeholder.user_id },
+        select: { id: true, username: true }
+      });
+
+      if (!user) {
+        return null;
+      }
+
+      // Calculate the distributed amount based on stake percentage
+      const sharePercentage = stakeholder.stake || 0;
+      const totalAmount = ticketTransactions * Number(event.cost);
+      const distributedAmount = (sharePercentage / 100) * totalAmount;
+
+      return {
+        user_id: user.id,
+        username: user.username,
+        sharePercentage: sharePercentage,
+        distributedAmount: distributedAmount
+      };
+    }));
+
+
+    const stekholderData = distributionData.filter(data => data !== null);
     const total = ticketTransactions * Number(event.cost)
   
     return NextResponse.json({ 
       total: total, 
-      ticketsSold: ticketTransactions
+      ticketsSold: ticketTransactions,
+      stekholderData
+
     }, { status: 200 });
 
   } catch (error) {
