6edf550e0a98612ea01fb6415fca25999ffeed90
Taombawkry <thomas.gondwe@outlook.com>
2024-06-23 13:27:34 +0200

    partners hack note page

---
+++ packages/dapp/src/app/(main)/partners/page.tsx
@@ -0,0 +1,31 @@
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkHtml from 'remark-html';
+import { fetchNoteContent, parseNoteContent } from '@/lib/hackMD';
+
+const Partners = async () => {
+  const noteId = 'thV0GdbvRba4LW4FF8S6iw';
+
+  try {
+    const note = await fetchNoteContent(noteId);
+    const parsedNote = parseNoteContent(note.content);
+
+    const processedContent = await unified()
+      .use(remarkParse)
+      .use(remarkHtml)
+      .process(parsedNote.content);
+    const contentHtml = processedContent.toString();
+
+    return (
+      <div>
+        <h1>{parsedNote.data.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+      </div>
+    );
+  } catch (error) {
+    console.error('Error fetching note content:', error);
+    return <div>Error loading note</div>;
+  }
+};
+
+export default Partners;
