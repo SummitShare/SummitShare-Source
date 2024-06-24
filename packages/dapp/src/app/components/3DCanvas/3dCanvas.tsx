6c4ce6fe1b0c95e171a819cc0e87946a05a085fc
mariojere <130987129+mariojere@users.noreply.github.com>
2024-06-24 01:57:14 +0000

    dynamic ehibit ppages and 3d objects

--- packages/dapp/src/app/components/3DCanvas/3dCanvas.tsx
+++ packages/dapp/src/app/components/3DCanvas/3dCanvas.tsx
@@ -1,29 +1,31 @@
+'use client'
 import React, { ReactNode } from "react";
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from "@react-three/drei";
-import { DirectionalLight } from "three"; // Import DirectionalLight
-import { Drum } from "./Drum";
+import { DirectionalLight } from "three";
 
+interface SummitShareCanvasProps {
+  children: ReactNode;
+}
 
-const SummitShareCanvas = ()=> {
+const SummitShareCanvas: React.FC<SummitShareCanvasProps> = ({ children }) => {
   return (
-    <div className="bg-gradient-to-t from-orange-600 to-orange-400  h-[410px] w-[500px] rounded-xl">
+    <div className="bg-primary-50 from-orange-600 to-orange-400 h-[360px] w-full rounded-[8px]">
       <Canvas
         frameloop="demand"
         shadows
-        camera={{ position: [10, 10, 10], fov: 45, near: 0.1, far: 1000 }} // Adjust camera position and parameters
+        camera={{ position: [0, 0, 10], fov: 45, near: 0.1, far: 1000 }} // Centered camera position
         gl={{ preserveDrawingBuffer: true }}
       >
         {/* Add Directional Light */}
         <directionalLight
-          intensity={20} // Adjust the intensity as needed
+          intensity={5} // Adjust the intensity as needed
           position={[5, 10, 5]} // Adjust the position as needed
-          castShadow // Enable shadow casting
         />
- 
-        <ambientLight intensity={10} />
 
-     <Drum/>
+        <ambientLight intensity={5} />
+
+        {children}
 
         {/* Add OrbitControls to enable camera rotation around the object */}
         <OrbitControls
