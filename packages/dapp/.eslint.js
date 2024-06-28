2514adf946b91e39c8df5f252663e8e3c9ebcf92
Taombawkry <thomas.gondwe@outlook.com>
2024-06-28 16:47:07 +0200

    task: linting + prettier config

--- packages/dapp/.eslint.js
+++ packages/dapp/.eslint.js
@@ -1,57 +1,77 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-      'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-      'plugin:jsx-a11y/recommended', // Uses the recommended rules from @eslint-plugin-jsx-a11y
-      'plugin:import/errors', // Uses the recommended rules from @eslint-plugin-import
-      'plugin:import/warnings',
-      'plugin:import/typescript',
-      'plugin:react-hooks/recommended',
-      'plugin:@next/next/recommended', // Uses the recommended rules from @next/eslint-plugin-next
-    ],
-    parserOptions: {
-      ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-      sourceType: 'module', // Allows for the use of imports
-      ecmaFeatures: {
-        jsx: true, // Allows for the parsing of JSX
-      },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
+    'prettier', // Add Prettier last
+  ],
+  plugins: ['prettier'], // Add Prettier plugin
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    settings: {
-      react: {
-        version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-      },
-      'import/resolver': {
-        typescript: {}, // This loads <rootdir>/tsconfig.json to eslint
-      },
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    rules: {
-      // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Ignore unused variables that start with an underscore
-      '@typescript-eslint/explicit-module-boundary-types': 'off', // Turn off explicit return type requirement
-      '@typescript-eslint/no-explicit-any': 'warn', // Warn on use of the any type
-      'react/prop-types': 'off', // Turn off prop-types as we use TypeScript for type checking
-      'react/react-in-jsx-scope': 'off', // React 17+ JSX Transform
-      'import/order': [
-        'error',
-        {
-          groups: [['builtin', 'external', 'internal']],
-          'newlines-between': 'always',
-        },
-      ], // Enforce import order
-      'react-hooks/exhaustive-deps': 'warn', // Warn for missing dependencies in useEffect
-      'react/jsx-key': 'error', // Error when missing key prop in iterators
-      'react/no-unescaped-entities': 'error', // Error for unescaped entities in JSX
-      '@next/next/no-async-client-component': 'error', // Prevent async client components
+    'import/resolver': {
+      typescript: {},
     },
-    overrides: [
+  },
+  rules: {
+    'prettier/prettier': 'error', // Ensure Prettier rules are enforced
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
       {
-        files: ['*.ts', '*.tsx'], // Apply these rules to TypeScript files only
-        rules: {
-          '@typescript-eslint/no-var-requires': 'off', // Allow require statements in TypeScript files
-        },
+        groups: [['builtin', 'external', 'internal']],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
       },
     ],
-  };
-  
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-key': 'error',
+    'react/no-unescaped-entities': 'error',
+    '@next/next/no-async-client-component': 'error',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+    'jsx-a11y/alt-text': [
+      'error',
+      {
+        elements: ['img', 'object', 'area', 'input[type="image"]'],
+        img: ['Image'],
+        object: ['Object'],
+        area: ['Area'],
+        'input[type="image"]': ['InputImage'],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+};
