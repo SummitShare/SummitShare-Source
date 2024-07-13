bde039ee69c8fd568b65e4fae00135e253d836db
Taombawkry <thomas.gondwe@outlook.com>
2024-07-13 12:28:08 +0200

    artifact NFT function update

--- packages/contracts/contracts/ArtifactNFT.sol
+++ packages/contracts/contracts/ArtifactNFT.sol
@@ -6,7 +6,6 @@ pragma solidity ^0.8.0;
 
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract ArtifactNFT is ERC721A, Ownable {
     string private baseURI;
--- packages/contracts/contracts/EventOrganizerService.sol
+++ packages/contracts/contracts/EventOrganizerService.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 /* Category: Smart Contract
    Purpose: Manages the organization and deployment of exhibits for events, integrating with the Museum contract and handling ArtifactNFTs. */
-
+   
 import "./Museum.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ArtifactNFT.sol";
