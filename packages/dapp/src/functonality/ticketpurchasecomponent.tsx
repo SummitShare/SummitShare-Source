6603dfe361b4c9077e0778c1a77039c1af63221f
Taombawkry <thomas.gondwe@outlook.com>
2024-07-28 13:30:57 +0200

    payment process pop up

--- packages/dapp/src/functonality/ticketpurchasecomponent.tsx
+++ packages/dapp/src/functonality/ticketpurchasecomponent.tsx
@@ -7,10 +7,17 @@ import { handleContractError } from '@/utils/dev/handleContractError';
 import useExhibit from '@/lib/useGetExhibitById';
 import { useSession } from 'next-auth/react';
 import Buttons from '@/app/components/button/Butons';
+import WalletStatus from './walletStatus';
+import Image from 'next/image';
 
 const TicketPurchaseComponent = ({ userAddress }: TicketPurchaseProps) => {
   const session = useSession();
   const [isVisible, setIsVisible] = useState(false);
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const [gasFees, setGasFees] = useState('0.00');
+  const [buttonType, setButtonType] = useState<'primary' | 'secondary' | 'tartary' | 'subTartary'>('primary');
+  const [buttonText, setButtonText] = useState('Pay');
+
   const user_id = session.data?.user.id;
 
   // Hardcoded exhibit ID for demo
@@ -19,8 +26,7 @@ const TicketPurchaseComponent = ({ userAddress }: TicketPurchaseProps) => {
 
   // State hooks for managing component state
   const [status, setStatus] = useState<string>('');
-  const [provider, setProvider] =
-    useState<ethers.providers.Web3Provider | null>(null);
+  const [provider, setProvider] = useState<ethers.providers.Web3Provider | null>(null);
   const [purchaseSuccessful, setPurchaseSuccessful] = useState<boolean>(false);
   const [purchaseFailed, setPurchaseFailed] = useState<boolean>(false);
   const exhibit = useExhibit(exhibitId);
@@ -35,6 +41,13 @@ const TicketPurchaseComponent = ({ userAddress }: TicketPurchaseProps) => {
       return () => clearTimeout(timer);
     }
   }, [status]);
+
+  useEffect(() => {
+    if (purchaseSuccessful) {
+      setIsPopupVisible(false);
+      setButtonType('secondary');
+    }
+  }, [purchaseSuccessful]);
 
   const createTicket = async () => {
     // Ensure HOST is read correctly, considering Next.js environment variables need to be prefixed with NEXT_PUBLIC_ if they are to be used on the client-side.
@@ -92,6 +105,7 @@ const TicketPurchaseComponent = ({ userAddress }: TicketPurchaseProps) => {
   if (!exhibit) {
     return <div>Loading or no Matching Exhibit Found.</div>;
   }
+
   // set ticket price from object pulled from subgraph
   const ticketPrice = exhibit.exhibitDetails[0]?.ticketPrice || '';
   //console.log("details:", exhibit);
@@ -101,6 +115,12 @@ const TicketPurchaseComponent = ({ userAddress }: TicketPurchaseProps) => {
   const ticketPriceFormatted = ethers.utils.formatUnits(ticketPriceWei, 6);
   const ticketPriceWithToken = `${ticketPriceFormatted} USDT`;
  // console.log("ticketPrice:", ticketPriceWithToken);
+
+ const calculateTotalPrice = () => {
+  const total = (parseFloat(ticketPriceFormatted) + parseFloat(gasFees));
+  return total.toFixed(2);
+};
+
 
   // Function to handle ticket purchase
   const purchaseTicket = async () => {
@@ -116,13 +136,15 @@ const TicketPurchaseComponent = ({ userAddress }: TicketPurchaseProps) => {
 
       // Execute token approval 
       setStatus('Approving token transfer...');
+      setButtonText('processing...');
       const gasLimitApprove = await estimateGas(usdcContract, 'approve', [CONTRACT_ADDRESSES.MuseumAdd, ticketPrice]);
       const approveTx = await usdcContract.approve(
         CONTRACT_ADDRESSES.MuseumAdd,
         ticketPrice,
         { gasLimit: gasLimitApprove }
       );
-      await approveTx.wait(2);
+      const approveReceipt =  await approveTx.wait();
+      const gasFeesApprove = ethers.utils.formatUnits(approveReceipt.gasUsed.mul(approveTx.gasPrice), 'ether')
       
       // Execute ticket purchase transaction
       setStatus('Purchasing ticket...');
@@ -132,15 +154,22 @@ const TicketPurchaseComponent = ({ userAddress }: TicketPurchaseProps) => {
         ticketPrice,
         { gasLimit: gasLimitPurchase }
       );
-      await purchaseTx.wait(2);
+      const purchaseReceipt = await purchaseTx.wait();
+      const gasFeesPurchase = ethers.utils.formatUnits(purchaseReceipt.gasUsed.mul(purchaseTx.gasPrice), 'ether');
+
+      const totalGasFees = parseFloat(gasFeesApprove) + parseFloat(gasFeesPurchase);
+      setGasFees(totalGasFees.toFixed(6));
 
       //State update after successful ticket purchase
       setPurchaseSuccessful(true);
       setStatus('Ticket purchased successfully!');
+      setButtonText('Pay');
     } catch (error: any) {
       console.error('Smart Contract Interaction Failed:', error);
       const friendlyMessage = handleContractError(error as any); // Typecasting
       setStatus(friendlyMessage);
+      setButtonText('Pay');
+
     }
   };
 
@@ -148,41 +177,114 @@ const TicketPurchaseComponent = ({ userAddress }: TicketPurchaseProps) => {
 
   const url = '${host}/api/v1/events/tickets/create';
 
-  // useEffect(() => {
-  //   const sendData = async () => {
-  //     try {
-  //       const response = await fetch(url, {
-  //         method: 'POST',
-  //         headers: {
-  //           'Content-Type': 'application/json'
-  //         },
-  //         body: JSON.stringify(wallet_address,event_id,user_id,eventLink)
-  //       });
-
-  //       if (!response.ok) {
-  //         throw new Error(`Error: ${response.status} - ${response.statusText}`);
-  //       }
-
-  //       const responseData = await response.json();
-  //       //console.log('Success:', responseData);
-  //     } catch (error) {
-  //       console.error('Failed to send data:', error);
-  //     }
-  //   };
-
-  //   sendData();
-  // }, [purchaseSuccessful]);
+  const togglePopup = () => {
+    setIsPopupVisible(!isPopupVisible);
+  };
 
   // Render component UI
   return (
     <>
-      <Buttons type="primary" size="large" onClick={purchaseTicket}>
-        {purchaseSuccessful ? 'Subscribed' : 'Subscribe'}
+      <Buttons 
+        type={buttonType} 
+        size="large" 
+        onClick={purchaseSuccessful ? () => window.open("https://oncyber.io/spaces/89cp8FpYgF5hgrHk1i3N", "_blank") : togglePopup} 
+        disabled={purchaseSuccessful}
+      >
+        {purchaseSuccessful ? 'View Exhibit' : 'Purchase'}
       </Buttons>
-
-      {/* Display current status */}
+  
+      {isPopupVisible && !purchaseSuccessful && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+          <div className="relative bg-white p-6 rounded-lg shadow-lg w-96">
+            <button
+              className="absolute top-0 right-0 mt-4 mr-4 text-gray-500 hover:text-gray-700"
+              onClick={togglePopup}
+            >
+              ✕
+            </button>
+            <h2 className="text-lg font-semibold mb-4 text-center">{exhibit.exhibitDetails[0]?.name}</h2>
+  
+            <div className="mb-4 text-center">
+              <label className="block text-sm font-medium text-gray-700">Ticket Price:</label>
+              <p className="font-bold flex justify-center items-center">
+                {ticketPriceWithToken}
+                <Image
+                  src="https://optimistic.etherscan.io/token/images/tether_32.png"
+                  alt="Tether"
+                  width={16}
+                  height={16}
+                  className="ml-2"
+                />
+              </p>
+            </div>
+  
+            <div className="mb-4 text-center">
+              <label className="block text-sm font-medium text-gray-700">Quantity:</label>
+              <p>1x</p>
+            </div>
+  
+            <div className="mb-4 text-center">
+              <label className="block text-sm font-medium text-gray-700">Address to:</label>
+              <WalletStatus />
+            </div>
+  
+            <div className="mb-4 text-center relative">
+              <label className="block text-sm font-medium text-gray-700">Total Price:</label>
+              <p className="font-bold flex justify-center items-center">  
+                {calculateTotalPrice()} USDT
+                <Image
+                  src="https://optimistic.etherscan.io/token/images/tether_32.png"
+                  alt="Tether"
+                  width={16}
+                  height={16}
+                  className="ml-2"
+                />
+              </p>
+  
+              <div className="relative inline-block group">
+                <small
+                  className="cursor-pointer text-black rounded-full border border-black bg-white"
+                  style={{
+                    padding: '2px 6px',
+                    display: 'inline-block',
+                    lineHeight: '1em',
+                    textAlign: 'center',
+                    fontSize: '12px',
+                  }}
+                >
+                  ?
+                </small>
+                <div
+                  className="absolute left-0 transform -translate-x-full mt-1 w-max bg-white text-gray-800 text-sm border border-gray-300 rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10"
+                  style={{ whiteSpace: 'pre-line', padding: '8px 12px' }}
+                >
+                  <div><b>Breakdown:</b> </div>
+                  {`Ticket Price: ${ticketPriceFormatted} USDT
+  Gas Fees: ${gasFees} USDT
+  (Gas fees are used to process the purchase onchain and do not go to us.)`}
+                </div>
+              </div>
+            </div>
+  
+            <Buttons type="primary" size="large" onClick={purchaseTicket}>
+              {buttonText}
+            </Buttons>
+          </div>
+        </div>
+      )}
+  
+      {purchaseSuccessful && (
+        <div className="fixed bottom-5 right-5 z-50">
+          <a href="https://oncyber.io/spaces/89cp8FpYgF5hgrHk1i3N" target="_blank" rel="noopener noreferrer">
+            <Buttons type="secondary" size="large">
+              View Exhibit
+            </Buttons>
+          </a>
+        </div>
+      )}
+  
       <div
-        className={`bg-green-500 border w-[90%] md:w-fit rounded-md p-3 fixed right-5 z-10 transition-transform duration-500 border-green-300 ${
+        className={`bg-green-500 border w-[90%] md:w-fit rounded-md p-3 fixed right-5 z-50 transition-transform duration-500 border-green-300 ${
           isVisible ? 'translate-y-0 bottom-5' : 'translate-y-full -bottom-20'
         }`}
       >
@@ -191,5 +293,5 @@ const TicketPurchaseComponent = ({ userAddress }: TicketPurchaseProps) => {
     </>
   );
 };
-
+  
 export default TicketPurchaseComponent;
